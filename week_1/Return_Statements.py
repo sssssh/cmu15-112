35ac057c0ec5ba3cacaea52ac7deab0b75775beb
Asher Guan <>
2017-10-16 17:23:02 +0800

    all mv to Functions

--- week_1/Return_Statements.py
+++
@@ -1,7 +0,0 @@
-def isPositive(x):
-    return (x > 0)
-
-
-print(isPositive(5))
-print(isPositive(-5))
-print(isPositive(0))
--- week_1/Return_ends_the_function_immediately.py
+++
@@ -1,7 +0,0 @@
-def isPositive(x):
-    print("Hello!")  # runs
-    return (x > 0)
-    print("Goodbye!")  # does not run ('dead code')
-
-
-print(isPositive(5))
