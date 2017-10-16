a23b6b593b1bbfe1cb8ce3c0333df8020ff03e65
Asher Guan <>
2017-10-16 18:24:25 +0800

    some builtin functions

---
+++ week_1/Data_and_Expressions/Some_Builtin_Functions.py
@@ -0,0 +1,12 @@
+print("Type conversion functions:")
+print(bool(0))  # convert to boolean (True or False)
+print(float(42))  # convert to a floating point number
+print(int(2.8))  # convert to an integer (int)
+
+
+print("And some basic math functions:")
+print(abs(-5))  # absolute value
+print(max(2, 3))  # return the max value
+print(min(2, 3))  # return the min value
+print(pow(2, 3))  # raise to the given power (pow(x, y) == x ** y)
+print(round(2.354, 1))  # round with the given number of digits
