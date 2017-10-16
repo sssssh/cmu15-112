e346287d5992ff44cfb495e70124bb5cdc9d23b6
Asher Guan <>
2017-10-17 00:26:00 +0800

    nth prime

---
+++ week_2/Loops/nthPrime.py
@@ -0,0 +1,17 @@
+from isPrime import isPrime
+
+
+def nthPrime(n):
+    found = 0
+    guess = 0
+    while found <= n:
+        guess += 1
+        if isPrime(guess):
+            found += 1
+    return guess
+
+
+# and let's see a list of the primes
+for n in range(10):
+    print(n, nthPrime(n))
+print("Done!")
