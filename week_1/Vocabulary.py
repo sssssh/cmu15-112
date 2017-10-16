80426e1b08892100b44cf33f66260be9013eb81b
Asher Guan <>
2017-10-16 16:34:52 +0800

    vocabulary

---
+++ week_1/Vocabulary.py
@@ -0,0 +1,23 @@
+x = 5
+
+
+def f(y, z):
+    result = x + y + z
+    return result
+
+
+print(f(1, 2))
+print(f(3, 4))
+
+
+# Vocabulary:
+#   global variable
+#   local variable
+#   statement
+#   expression
+#   function definition (or declaration)
+#   function call
+#   parameter (or 'formal parameter')
+#   argument
+#   return value
+#   return type
