c45b534a8ec8981d2d9a149923a5a74c63a8c3eb
Asher Guan <>
2017-10-16 18:32:02 +0800

    integer division

---
+++ week_1/Data_and_Expressions/Integer_Division.py
@@ -0,0 +1,8 @@
+print("The / operator does 'normal' float division:")
+print(" 5/3  =", (5 / 3))
+print()
+print("The // operator does integer division:")
+print(" 5//3 =", (5 // 3))
+print(" 2//3 =", (2 // 3))
+print(" -1//3 =", (-1 // 3))
+print(" -4//3 =", (-4 // 3))
