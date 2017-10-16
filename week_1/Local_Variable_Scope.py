1cf3abce968920635053cff4be33c2b7f8bd74bd
Asher Guan <>
2017-10-16 17:14:10 +0800

    complex one

--- week_1/Local_Variable_Scope.py
+++ week_1/Local_Variable_Scope.py
@@ -9,3 +9,22 @@ def g(x):
 
 
 print(g(2))
+
+
+def f(x):
+    print("In f, x =", x)
+    x += 7
+    return round(x / 3)
+
+
+def g(x):
+    x *= 10
+    return 2 * f(x)
+
+
+def h(x):
+    x += 3
+    return f(x + 4) + g(x)
+
+
+print(h(f(1)))
