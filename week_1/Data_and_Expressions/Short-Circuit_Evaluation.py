82944d0c4d78ae7154d946a42f564f64843aa3d5
Asher Guan <>
2017-10-16 19:14:02 +0800

    yet anoter example

--- week_1/Data_and_Expressions/Short-Circuit_Evaluation.py
+++ week_1/Data_and_Expressions/Short-Circuit_Evaluation.py
@@ -18,3 +18,22 @@ print(yes() and crash())
 print(yes() or crash())
 print(crash() or yes())
 print(no() or crash())
+
+
+def isPositive(n):
+    result = (n > 0)
+    print("isPositive(",n,") =", result)
+    return result
+
+
+def isEven(n):
+    result = (n % 2 == 0)
+    print("isEven(",n,") =", result)
+    return result
+
+
+print("Test 1: isEven(-4) and isPositive(-4))")
+print(isEven(-4) and isPositive(-4)) # Calls both functions
+print("----------")
+print("Test 2: isEven(-3) and isPositive(-3)")
+print(isEven(-3) and isPositive(-3)) # Calls only one function!
