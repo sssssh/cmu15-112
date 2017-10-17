2ad839cbd0833408104b8ef03a6b3ef0e621c200
Asher Guan <>
2017-10-17 17:22:35 +0800

    word search

---
+++ week_5/2d_Lists/wordSearch1.py
@@ -0,0 +1,50 @@
+def wordSearch(board, word):
+    (rows, cols) = (len(board), len(board[0]))
+    for row in range(rows):
+        for col in range(cols):
+            result = wordSearchFromCell(board, word, row, col)
+            if result != None:
+                return result
+    return None
+
+
+def wordSearchFromCell(board, word, startRow, startCol):
+    for dir in range(8):
+        result = wordSearchFromCellInDirection(board, word,
+                                               startRow, startCol, dir)
+        if (result != None):
+            return result
+    return None
+
+
+def wordSearchFromCellInDirection(board, word, startRow, startCol, dir):
+    (rows, cols) = (len(board), len(board[0]))
+    dirs = [ (-1, -1), (-1, 0), (-1, +1),
+             ( 0, -1),          ( 0, +1),
+             (+1, -1), (+1, 0), (+1, +1) ]
+    dirNames = [ "up-left"  ,   "up", "up-right",
+                 "left"     ,         "right",
+                 "down-left", "down", "down-right" ]
+    (drow,dcol) = dirs[dir]    
+    for i in range(len(word)):
+        row = startRow + i*drow
+        col = startCol + i*dcol
+        if ((row < 0) or (row >= rows) or
+            (col < 0) or (col >= cols) or
+            (board[row][col] != word[i])):
+            return None
+    return (word, (startRow, startCol), dirNames[dir])
+
+
+def testWordSearch():
+    board = [ [ 'd', 'o', 'g' ],
+              [ 't', 'a', 'c' ],
+              [ 'o', 'a', 't' ],
+              [ 'u', 'r', 'k' ],
+            ]
+    print(wordSearch(board, "dog")) # ('dog', (0, 0), 'right')
+    print(wordSearch(board, "cat")) # ('cat', (1, 2), 'left')
+    print(wordSearch(board, "tad")) # ('tad', (2, 2), 'up-left')
+    print(wordSearch(board, "cow")) # None
+
+testWordSearch()
