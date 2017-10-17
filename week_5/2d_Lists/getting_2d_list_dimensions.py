09498e3fa62a360a16dfede7b6170a048e0f733c
Asher Guan <>
2017-10-17 16:47:29 +0800

    get 2d list is value

---
+++ week_5/2d_Lists/getting_2d_list_dimensions.py
@@ -0,0 +1,9 @@
+# Create an "arbitrary" 2d List
+a = [[2, 3, 5], [1, 4, 7 ]]
+print("a = ", a)
+
+# Now find its dimensions
+rows = len(a)
+cols = len(a[0])
+print("rows =", rows)
+print("cols =", cols)
