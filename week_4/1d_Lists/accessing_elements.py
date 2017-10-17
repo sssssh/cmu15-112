90b555b7fcfadf586e6c5f98c0d40ea8016dd56a
Asher Guan <>
2017-10-17 13:01:25 +0800

    indexing slicing

---
+++ week_4/1d_Lists/accessing_elements.py
@@ -0,0 +1,15 @@
+a = [2, 3, 5, 7, 11, 13]
+print("a        =", a)
+
+# Access non-negative indexes
+print("a[0]     =", a[0])
+print("a[2]     =", a[2])
+
+# Access negative indexes
+print("a[-1]    =", a[-1])
+print("a[-3]    =", a[-3])
+
+# Access slices a[start:end:step]
+print("a[0:2]   =", a[0:2])
+print("a[1:4]   =", a[1:4])
+print("a[1:6:2] =", a[1:6:2])
