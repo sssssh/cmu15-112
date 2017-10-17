1e0dd463af680f319c2528bede47d086320fc3fd
Asher Guan <>
2017-10-17 10:29:24 +0800

    string methods

--- week_3/_Strings/Some_String_Methods.py
+++ week_3/_Strings/Some_String_Methods.py
@@ -21,3 +21,34 @@ def printTable():
 
 
 printTable()
+
+
+# 2 String edits: lower(), upper(), replace(), strip()
+print("This is nice. Yes!".lower())
+print("So is this? Sure!!".upper())
+print("   Strip removes leading and trailing whitespace only    ".strip())
+print("This is nice.  Really nice.".replace("nice", "sweet"))
+print("This is nice.  Really nice.".replace("nice", "sweet", 1))  # count = 1
+
+print("----------------")
+s = "This is so so fun!"
+t = s.replace("so ", "")
+print(t)
+print(s)  # note that s is unmodified (strings are immutable!)
+
+
+# 3 Substring search: count(), startswith(), endswith(), find(), index()
+print("This is a history test".count("is")) # 3
+print("This IS a history test".count("is")) # 2
+print("-------")
+print("Dogs and cats!".startswith("Do"))    # True
+print("Dogs and cats!".startswith("Don't")) # False
+print("-------")
+print("Dogs and cats!".endswith("!"))       # True
+print("Dogs and cats!".endswith("rats!"))   # False
+print("-------")
+print("Dogs and cats!".find("and"))         # 5
+print("Dogs and cats!".find("or"))          # -1
+print("-------")
+print("Dogs and cats!".index("and"))        # 5
+print("Dogs and cats!".index("or"))         # crash!
