ec0355eaadde89c3f95b724950fd03b60eed3fb2
Asher Guan <>
2017-10-17 09:47:36 +0800

    string module

---
+++ week_3/_Strings/Some_String_Constants.py
@@ -0,0 +1,13 @@
+import string
+
+
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print('--------------------')
+print(string.ascii_uppercase)
+print(string.digits)
+print('--------------------')
+print(string.punctuation)
+print(string.printable)
+print('--------------------')
+print(string.whitespace)
