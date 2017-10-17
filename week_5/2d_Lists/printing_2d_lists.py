441c05c23c3a159be210636f9bf1f7e80dcb855b
Asher Guan <>
2017-10-17 17:01:19 +0800

    print 2d lists a bit nicer

---
+++ week_5/2d_Lists/printing_2d_lists.py
@@ -0,0 +1,43 @@
+# Helper function for print2dList
+# This finds the maximum length of the string representation of any item in the 2d list
+
+
+def maxItemLength(a):
+    maxLen = 0
+    rows = len(a)
+    cols = len(a[0])
+    for row in range(rows):
+        for col in range(cols):
+            maxLen = max(maxLen, len(str(a[row][col])))
+    return maxLen
+
+
+# Because Python prints 2d lists on one row,
+# we might want to write our own function
+# that prints 2d lists a bit nicer.
+def print2dList(a):
+    if (a == []):
+        # So we don't crash accessing a[0]
+        print([])
+        return
+    rows = len(a)
+    cols = len(a[0])
+    fieldWidth = maxItemLength(a)
+    print("[ ", end="")
+    for row in range(rows):
+        if (row > 0):
+            print("\n  ", end="")
+        print("[ ", end="")
+        for col in range(cols):
+            if (col > 0):
+                print(", ", end="")
+            # The next 2 lines print a[row][col] with the given fieldWidth
+            formatSpec = "%" + str(fieldWidth) + "s"
+            print(formatSpec % str(a[row][col]), end="")
+        print(" ]", end="")
+    print("]")
+
+
+# Let's give the new function a try!
+a = [[1, 2, 3], [4, 5, 67]]
+print2dList(a)
